163a4d9cf1049d94bab18de7f6a38ca3af14e439
Rituraj Singh <riturajsingh@riturajs-mbp.myfiosgateway.com>
2022-04-19 17:53:14 -0400

    added kmeans

--- feature_extraction/kmeans.py
+++ feature_extraction/kmeans.py
@@ -1,10 +1,40 @@
 import numpy as np
 import pandas as pd
+from sklearn.cluster import KMeans
+from collections import Counter
 
 feature_data = np.load('../tfidf_features.npz')
 
 tfidf_vectors = feature_data['name1']
 user_labels = feature_data['name2']
 
-print(len(tfidf_vectors))
-print(len(user_labels))
+n_clusters = 5
+kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+kmeans.fit(tfidf_vectors)
+
+#labels assigned to each tweet
+clusters = kmeans.labels_
+print("len = ", len(clusters))
+counts = Counter(clusters)
+print(counts)
+
+df = pd.read_csv('../cleaned_tweets.csv', index_col=[0])
+print("df len", len(df))
+df.columns = ['tweet', 'user']
+
+all_tweets_list = df['tweet']
+
+cluster_tweets = []
+for i in range(n_clusters):
+    cluster_tweets.append([])
+
+for i in range(len(all_tweets_list)):
+    cluster_label = clusters[i]
+    cluster_tweets[cluster_label].append(all_tweets_list[i])
+
+
+for i in range(len(cluster_tweets)):
+    print("CLUSTER ", i)
+    print("=========\n")
+    for j in range(10):
+        print(cluster_tweets[i][j])
